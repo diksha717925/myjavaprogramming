a05bde275caec68d0805a3391ac1c3c57cbbfcf4
diksha717925 <dikshadhannure@gmail.com>
2025-09-03 21:51:23 +0530

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,25 @@
+package Myjava;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+
+			    // DataTypes
+					System.out.println("There are two types of datatypes.");
+					System.out.println("1. Primtive Datatype.");
+					System.out.println("2. Non-Primitive Datatypes.");
+					System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+					System.out.println("Deep to 1. Primitive Data type.");
+					System.out.println("The primitive datatype is classified into two types 1. Number and 2.Non-Numeric");
+					System.out.println("(Primitive datatype), 1. Numeric: is classified into TWo types 1. cher, 2.boolean");
+					System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+					System.out.println("(Primitive datatype), 2. Non-Numeric : is classified into 6 types 1. Byte, 2.Short, 3.int, 4.float, 5.double, 6.long");
+					System.out.println("(Primitive datatype), 1. Numeric: is classified into TWo types 1. cher, 2.boolean");
+					System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+					System.out.println("2. Non-Primitive Datatype: these are the DataStructure.");
+					System.out.println("they are 1.Array, 2.String, 3.Map, 4.set, 5.Tree, 6.Queue, 7.Stack.");
+
+			  }
+			
+
+}
