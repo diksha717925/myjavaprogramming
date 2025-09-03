53fefc693e6edeb07a7d93b5623f03404592abe0
diksha717925 <dikshadhannure@gmail.com>
2025-09-03 21:52:21 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,17 @@
+package Myjava;
+
+public class Operators {
+	public static void main(String[]orgs) {
+    System.out.print("Operators in Java");
+	
+    System.out.println("Arthemitic and Relations Operators");
+    System.out.println("Logical Operator");
+    int x=9; int y=10;
+    
+    System.out.println(x<y); //9<10 :true
+    System.out.println(x>y); //9>10 :false
+    System.out.println(x!=y); //9=10 :tuue
+    System.out.println(x<=y); //9<=10 :true
+	}
+    
+}
