e073dab09e90eba1a4300215a2c4527d9ce3129d
diksha717925 <dikshadhannure@gmail.com>
2025-09-03 21:54:59 +0530

    Add files via upload

---
+++ java5.java
@@ -0,0 +1,27 @@
+package Myjava;
+
+public class Main {
+
+		public static void main(String[] args) {
+		    //first number
+			int num1=17;
+			//second number
+			int num2=25;
+			
+		//modulus operation
+	    int mod=num1%num2; 
+		//display the result
+		System.out.println("modulus of 17 and 25: "+ mod);
+		//division operation
+		int div=num1/num2;
+		//display the result
+		System.out.println("division of 17 and 25:"+div);
+		//addition operation
+		int add=num1+num2;
+		System.out.println("addition of 17 and 25:"+ add);
+		//subtraction operation
+		int sub=num2-num1;
+		System.out.println("subtraction of 17 and 25:"+ sub);
+
+		}
+}
