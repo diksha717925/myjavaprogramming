aa4bd38713191863e30d5caf7b3d39d063c412d0
diksha717925 <dikshadhannure@gmail.com>
2025-09-03 21:49:46 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,28 @@
+package Myjava;
+
+public class Bits {
+	public static void main(String[]args) {
+		
+	System.out.println("Bitwise and Operators");
+	int a=0;
+    int b=0;
+	System.out.println("when a=0 & b=0"+": "+(a&b)); //0  
+	
+	System.out.println("Bitwise OR Operators");
+	int a1=1;
+    int b2=0;
+	System.out.println("when a1=0 & b2=0"+": "+(a1|b2)); //0  
+	
+	 
+	}
+
+   
+
+
+
+
+
+
+
+
+}
