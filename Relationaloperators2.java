85eb7eacaeaf8447c2541f3afc103569f3027533
diksha717925 <dikshadhannure@gmail.com>
2025-09-11 00:07:35 +0530

    Add files via upload

---
+++ Relationaloperators2.java
@@ -0,0 +1,21 @@
+package Myjava;
+
+
+	public class Relationaloperators2{
+
+
+				public static void main(String[]orgs) {
+			    System.out.print("Operators in Java");
+			    System.out.println("Relational operators");
+			    
+			    int x=9; int y=10;
+			    System.out.println(x<y); //9<10 :true
+			    System.out.println(x>y); //9>10 :false
+			    System.out.println(x!=y); //9=10 :true
+			    System.out.println(x<=y); //9<=10 :true
+			    System.out.println(x>=y); //9>=10 :false
+				}
+	         }
+
+
+
