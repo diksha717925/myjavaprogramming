c2b17d255259eb1d6cfb75a76d1cd93951955190
diksha717925 <dikshadhannure@gmail.com>
2025-09-11 00:06:01 +0530

    Add files via upload

---
+++ termsandconditions.java
@@ -0,0 +1,14 @@
+package Myjava;
+
+public class termsandconditions {
+    public static void main(String[] args) {
+        // variables in java:
+        System.out.println("1. Variables are the storage containers, which are non-static.");
+        System.out.println("2. Variables must start from lowercase.");
+        System.out.println("3. Variables are valid with only underscore and other special symbols are not valid.");
+        System.out.println("4. Variables can't be assigned any reserved words or keywords.");
+        System.out.println("5. Variables must be a meaningful word.");
+        System.out.println("6. Variables cannot be duplicate.");
+    }
+}
+    
